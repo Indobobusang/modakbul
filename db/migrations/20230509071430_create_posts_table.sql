8aa571fcf0e21bf8dcc0f5b44a188412c6157a3e
박준우 <junwoo@bagjun-uui-MacBookPro.local>
2023-05-15 15:34:35 +0900

    MODIFY [박준우] posts테이블에 위도 경도 컬럼 추가

--- db/migrations/20230509071430_create_posts_table.sql
+++ db/migrations/20230509071430_create_posts_table.sql
@@ -5,6 +5,8 @@ CREATE TABLE posts (
   content VARCHAR(3000),
   user_id INT,
   post_category_id INT,
+  longitude DECIMAL(18,14),
+  latitude DECIMAL(18,14),
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 );
